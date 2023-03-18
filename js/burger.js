b7405d04afcfacb720ca1d1083d1d25d49377ed4
anywalkerjr <109415906+anywalkerjr@users.noreply.github.com>
2023-03-18 21:49:32 +0300

    Update burger.js

--- js/burger.js
+++ js/burger.js
@@ -1,82 +1,123 @@
-$(document).ready(function($){ 
-    let condition = true;
-    function forwards() {
-        anime({
-            targets: '.hburgerMenu',
-            translateX: ['-50vw', '0'],
-            color: 'white',
-            easing: 'easeInOutQuad',
-            direction: 'alternate',
-            duration: 700,
-            loop: false
-        });
+$(document).ready(function($) {
+	let condition = true;
+	function forwards() {
+		anime({
+			targets: '.leftMenu',
+			translateX: [ '-50vw', '0' ],
+			color: 'white',
+			easing: 'easeInOutQuad',
+			direction: 'alternate',
+			duration: 700,
+			loop: false
+		});
+		anime({
+			targets: '.hburgerMenu',
+			translateX: [ '-50vw', '0' ],
+			color: 'white',
+			easing: 'easeInOutQuad',
+			direction: 'alternate',
+			duration: 700,
+			loop: false
+		});
 
-        anime({
-            targets: '.hburgerMenuIcon',
-            rotate: 90,
-            easing: 'easeInOutQuad',
-            direction: 'alternate',
-            duration: 500,
-            loop: false
-        });
-        anime({
-            targets: '.stickHburger',
-            rotate: -90,
-            easing: 'easeInOutQuad',
-            direction: 'alternate',
-            duration: 1000,
-            loop: false
-        });
-        condition = false
-    }
+		anime({
+			targets: '#stick1',
+			rotate: -45,
 
-    function backwards() {
-        anime({
-            targets: '.hburgerMenu',
-            translateX: ['0', '-50vw'],
-            easing: 'easeInOutQuad',
-            direction: 'alternate',
-            duration: 700,
-            loop: false
-        });
-        anime({
-            targets: '.hburgerMenuIcon',
-            rotate: 0,
-            easing: 'easeInOutQuad',
-            direction: 'alternate',
-            duration: 500,
-            loop: false
-        });
-        anime({
-            targets: '.stickHburger',
-            rotate: 0,
-            easing: 'easeInOutQuad',
-            direction: 'alternate',
-            duration: 1000,
-            loop: false
-        });
-        condition = true;
-    }
+			translateX: '-11px',
+			easing: 'easeInOutQuad',
+			direction: 'alternate',
+			duration: 650,
+			loop: false
+		});
+		anime({
+			targets: '#stick2',
+			opacity: 0,
+			easing: 'easeInOutQuad',
+			direction: 'alternate',
+			duration: 650,
+			loop: false
+		});
+		anime({
+			targets: '#stick3',
+			rotate: 45,
 
-    $('.hburgerMenuIcon').click(function () {
-        if (condition) {
-            forwards();
-        } else {
-            backwards();
-        }
-    });
+			translateX: '-11px',
+			easing: 'easeInOutQuad',
+			direction: 'alternate',
+			duration: 650,
+			loop: false
+		});
+		condition = false;
+	}
 
-    let start = 0;
-    let end = 0;
-    $('body').on('touchstart', function (event) {
-        start = event.originalEvent.touches[0].pageX;
-    })
-    $('body').on('touchend', function (event) {
-        end = event.originalEvent.changedTouches[0].pageX;
-        if (end - start >= 100 && condition) {
-            forwards();
-        } else if (start - end >= 100 && !condition) {
-            backwards();
-        }
-    });
-})
+	function backwards() {
+		anime({
+			targets: '.leftMenu',
+			translateX: [ '0', '-50vw' ],
+			easing: 'easeInOutQuad',
+			direction: 'alternate',
+			duration: 700,
+			loop: false
+		});
+		anime({
+			targets: '.hburgerMenu',
+			translateX: [ '0', '-50vw' ],
+			easing: 'easeInOutQuad',
+			direction: 'alternate',
+			duration: 700,
+			loop: false
+		});
+		anime({
+			targets: '#stick1',
+			rotate: 0,
+
+			translateX: '0px',
+			easing: 'easeInOutQuad',
+			direction: 'alternate',
+			duration: 650,
+			loop: false
+		});
+		anime({
+			targets: '#stick2',
+			opacity: 1,
+			easing: 'easeInOutQuad',
+			direction: 'alternate',
+			duration: 650,
+			loop: false
+		});
+		anime({
+			targets: '#stick3',
+			rotate: 0,
+
+			translateX: '0px',
+			easing: 'easeInOutQuad',
+			direction: 'alternate',
+			duration: 650,
+			loop: false
+		});
+		condition = true;
+	}
+
+	$('.hburgerMenuIcon').click(function() {
+		if (condition) {
+			forwards();
+		} else {
+			backwards();
+		}
+	});
+
+	let start = 0;
+	let end = 0;
+	$('body').on('touchstart', function(event) {
+		start = event.originalEvent.touches[0].pageX;
+	});
+	$('body').on('touchend', function(event) {
+		end = event.originalEvent.changedTouches[0].pageX;
+		if (end - start >= 100 && condition) {
+			forwards();
+		} else if (start - end >= 100 && !condition) {
+			backwards();
+		}
+	});
+});
